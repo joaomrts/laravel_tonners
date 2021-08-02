2d6abbafdec29dac5c236732e81afd2c2d38357b
joaomrts <joaobatista0811@gmail.com>
2021-08-02 16:59:48 -0300

    auth service indexImpressora_correção3

--- app/Http/Controllers/ImpressoraController.php
+++ app/Http/Controllers/ImpressoraController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Impressora;
 use App\Models\ImpressorasXavantes;
+use App\Models\ImpressorasMendesJr;
 use App\Http\Requests\StoreUpdateImpressora;
 use PDF;
 
@@ -14,6 +15,7 @@ class ImpressoraController extends Controller
     public function indexImpressora()
     {
         $impressoras = Impressora::all();
+        $impressorasXavantess = ImpressorasXavantes::all();
         $impressorasMendesJrs = ImpressorasMendesJr::all();
 
         return view('impressora.indexImpressora', ['impressoras' => $impressoras, 'impressorasXavantess' => $impressorasXavantess, 'impressorasMendesJrs' => $impressorasMendesJrs]);
