9403ca625f5a85bd5f9cd2940ff73f78a122f24a
joaomrts <joaobatista0811@gmail.com>
2021-08-05 13:57:48 -0300

    campos de busca1

--- resources/views/impressora/indexImpressora.blade.php
+++ resources/views/impressora/indexImpressora.blade.php
@@ -20,7 +20,7 @@
     <div class="dashboard-tinta-container">
         <a href="/indexCilindro" id="show" style="margin-left: 5px" class="btn btn-dark"><ion-icon name="film-outline"></ion-icon> Cilindros</a></h5>
         <a href="/indexTinta" id="show" style="margin-left: 5px" class="btn btn-dark"><ion-icon name="color-palette-outline"></ion-icon> Tintas</a></h5>
-        <a href="/indexTonner"  style="margin-left: 320px" class="btn btn-dark"><ion-icon name="repeat-outline"></ion-icon> Tonners</a>
+        <a href="/indexTonner"  style="margin-left: 300px" class="btn btn-dark"><ion-icon name="repeat-outline"></ion-icon> Tonners</a>
     </div>
     <a href="/" id="show" style="margin-right: 5px" class="btn btn-dark"><ion-icon name="home-outline"></ion-icon> Início</a></h5>
     <h1> Minas Gerais
