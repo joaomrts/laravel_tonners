f21f3375125e6bb11bd07c8779a8f237f46657f2
joaomrts <joaobatista0811@gmail.com>
2021-08-16 16:50:20 -0300

    alteranção pagina de login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,108 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+        <title>Souza e Cambos Login</title>
+        <!-- Fonte do Google -->
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Open+Sans');
+        </style>
+
+        <!-- CSS Bootstrap -->
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <!-- CSS da aplicação -->
+
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+
+        <link rel="icon" type="favicon" href="/img/logo.png" />
+
+    </head>
+    <body>
+        <header>
+            <nav class="">
+                <div class="collapse navbar-collapse" id="guest">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/logo.png" alt="logo">
+                    </a>
+                    <h1>Controle de T.I.</h1>
+                    <ul class="navbar-nav">
+                        @guest
+                        <li><ion-icon name="person-circle-outline" style="margin-right: 5px"></ion-icon>Olá Visitante</li>
+
+                        @endguest
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if (session('msg'))
+                    <p class="msg">{{ session('msg') }}</p></p>
+                    @endif
+                    @yield('content')
+                </div>
             </div>
-        </div>
-    </div>
-</x-app-layout>
+        </main>
+        <footer>
+            <p>Souza e Cambos Confecções &copy; 2021</p>
+        </footer>
+        <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    </body>
+</html>
+
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <a href="/" class="navbar-brand">
+                <img src="/img/logo.png" alt="Logo">
+            </a>
+        </x-slot>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div>
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Senha') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <x-jet-checkbox id="remember_me" name="remember" />
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Manter Conectado') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('register'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Não tem uma conta? Cadastre-se') }}
+                    </a>
+                @endif
+
+                <x-jet-button class="ml-4">
+                    {{ __('Entrar') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
