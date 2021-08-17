404c95c17da15c8fb8c8a6daac520911c3af3cf2
joaomrts <joaobatista0811@gmail.com>
2021-08-17 08:23:27 -0300

    menu dropdown navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,6 +14,10 @@
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
         <!-- CSS da aplicação -->
 
         <link rel="stylesheet" href="/css/styles.css">
@@ -32,19 +36,26 @@
                     <h1>Controle de T.I.</h1>
                     <ul class="navbar-nav">
                         @auth
-                        <li><ion-icon name="person-circle-outline" style="margin-right: 5px"></ion-icon>Olá {{ Auth::user()->name }}</li>
-                         <li class="nav-form">
-                            <form action="/logout" method="POST">
-                            @csrf
-                            <a href="/logout"
-                            class="nav-link"
-                            id="form-logout"
-                            onclick="event.preventDefault();
-                            this.closest('form').submit();">
-                            Sair
-                            </a>
-                            </form>
-                         </li>
+                        <div class="btn-group" style="margin-right: 20px">
+                            <button type="button" class="btn btn-outline-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <ion-icon name="person-circle-outline" style="margin-right: 5px"></ion-icon>Olá {{ Auth::user()->name }}
+                            </button>
+                            <div class="dropdown-menu" style="margin-right: 15px">
+                                <li class="nav-form">
+                                    <form action="/logout" method="POST">
+                                    @csrf
+                                    <a href="/logout"
+                                    style="text-align: center"
+                                    class="nav-link"
+                                    id="form-logout"
+                                    onclick="event.preventDefault();
+                                    this.closest('form').submit();">
+                                    Sair do sistema
+                                    </a>
+                                    </form>
+                                 </li>
+                            </div>
+                          </div>
                         @endauth
                     </ul>
                 </div>
