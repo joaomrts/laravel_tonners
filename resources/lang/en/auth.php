ad1e16bdb433ac3470b07ce6c08cdba0514203d6
joaomrts <joaobatista0811@gmail.com>
2021-08-18 07:54:24 -0300

    tradução de mensagens

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'password' => 'Senha Incorreta.',
+    'throttle' => 'Várias tentativas de login. Por favor, tente em :seconds segundos.',
 
 ];
