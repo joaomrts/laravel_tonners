ebfa6985b9aac4180f6c19089a8b09c8b208e304
joaomrts <joaobatista0811@gmail.com>
2021-08-23 11:51:57 -0300

    adicionando botoes manutencao e estilizando css1

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,7 +20,8 @@
 
         <!-- CSS da aplicação -->
 
-
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
 
         <link rel="icon" type="favicon" href="/img/logo.png" />
 
